4a6ff8e1ddd88c015b70b6029e9a84737897ae4d
JulianMiranda <jmirandauria@gmail.com>
2024-07-13 17:34:31 -0500

    All path

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {
+	BrowserRouter as Router,
+	Route,
+	Routes,
+	Navigate
+} from 'react-router-dom';
 import Home from './pages/Home';
 import AddProduct from './pages/AddProduct';
 import EditProduct from './pages/EditProduct';
@@ -13,6 +18,7 @@ const App: React.FC = () => {
 				<Route path="/" element={<Home />} />
 				<Route path="/add-product" element={<AddProduct />} />
 				<Route path="/edit-product/:id" element={<EditProduct />} />
+				<Route path="*" element={<Navigate to="/" />} />
 			</Routes>
 		</Router>
 	);
